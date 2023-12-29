e5ecf473120341895f7e6caf14f7da79c9bce6d6
samirfaritov <samirfaritov6@gmail.com>
2023-12-29 12:07:56 +0500

    pochti maket shud

--- js/birthday.js
+++ js/birthday.js
@@ -1,3 +1,9 @@
 window.addEventListener('click', (e) => {
     e.preventDefault()
 })
+
+const clickBtn = document.querySelector('.clickBtn')
+
+clickBtn.addEventListener('click', () => {
+    location.href = '/html/korzina.html'
+})
