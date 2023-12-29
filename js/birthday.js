94323bf15860349ebb46ec4307f73005e687839e
samirfaritov <samirfaritov6@gmail.com>
2023-12-29 16:50:32 +0500

    makket finish

--- js/birthday.js
+++ js/birthday.js
@@ -7,3 +7,15 @@ const clickBtn = document.querySelector('.clickBtn')
 clickBtn.addEventListener('click', () => {
     location.href = '/html/korzina.html'
 })
+
+const glav = document.querySelector('.glav')
+
+glav.addEventListener('click', () => {
+    location.href = '/index.html'
+})
+
+const kat = document.querySelector('.kat')
+
+kat.addEventListener('click', () => {
+    location.href = '/html/katalog.html'
+})
---
+++ js/korzina.js
@@ -0,0 +1,15 @@
+window.addEventListener('click', (e) => {
+    e.preventDefault()
+})
+
+const glav = document.querySelector('.glav')
+
+glav.addEventListener('click', () => {
+    location.href = '/index.html'
+})
+
+const ver = document.querySelector('.pp')
+
+ver.addEventListener('click', () => {
+    location.href = '/html/tovari.html'
+})
