23f3198988d4b881acee80a4fb3d1b18f87c887f
samirfaritov <samirfaritov6@gmail.com>
2024-01-03 11:00:29 +0500

    clear finish

--- js/main.js
+++ js/main.js
@@ -20,3 +20,30 @@ gal.addEventListener('click', () => {
     console.log('click');
     location.href = '/html/galareya.html'
 })
+
+const clear = document.querySelector('.clear')
+const bbq = document.querySelector('.bbq')
+const menu = document.querySelector('.menu')
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const pop = document.querySelector('.pop')
+const logoBtn = document.querySelector('.logoBtn')
+
+
+clear.addEventListener('click', () => {
+    bbq.style.display = 'none'
+    menu.style.backgroundColor = '#fff'
+    right.style.display = 'block'
+    left.style.display = 'block'
+    logoBtn.style.display = 'block'
+    pop.style.color = '#000'
+})
+
+menu.addEventListener('click', () => {
+    bbq.style.display = 'block'
+    menu.style.backgroundColor = '#FEDDFF'
+    right.style.display = 'none'
+    left.style.display = 'none'
+    logoBtn.style.display = 'none'
+    pop.style.color = 'transparent'
+})
